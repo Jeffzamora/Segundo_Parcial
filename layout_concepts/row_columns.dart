81f95e20a3f998b462cc4fba76ac076542b7e856
Jefferson Zamora <jeffzamorachiqui97@gmail.com>
2023-05-04 10:50:46 -0600

    codelag4,5

---
+++ layout_concepts/row_columns.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        BlueBox(),
+        BlueBox(),
+        BlueBox(),
+      ],
+    );
+  }
+}
